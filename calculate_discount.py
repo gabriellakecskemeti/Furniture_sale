70377e956bb66b9d47811ae8d5e10a0d2463d03c
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-02-10 23:24:31 +0100

    first test case

--- calculate_discount.py
+++ calculate_discount.py
@@ -3,5 +3,5 @@ from decimal import Decimal
 
 
 def calculate_discount(day: date, total: Decimal):
-    discount= Decimal("0")
+    discount = Decimal("0")
     return discount
--- calculate_discount_test.py
+++ calculate_discount_test.py
@@ -2,5 +2,16 @@ from calculate_discount import calculate_discount
 
 from datetime import date
 from decimal import Decimal
+import pytest
+
+_test_data_before_advent = [
+    pytest.param(date(2021, 11, 30), Decimal("0.01"), Decimal("0"), id="1:Date in 2021 Advent, first tier")
+]
 
 
+@pytest.mark.parametrize("day,total,expected", _test_data_before_advent)
+def test_calculate_discount_before_advent(day: date, total: Decimal, expected: Decimal):
+    # Act
+    actual = calculate_discount(day, total)
+    # Assert
+    assert actual == expected
